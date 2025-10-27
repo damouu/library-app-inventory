f0b7fedb21c836b0d6460e8c390ba810e1be7888
damouu <mouadsehbaoui@gmail.com>
2025-10-27 11:53:19 +0100

    #SD-232 Done Unassigned TestsPassed Done
    refactor: MemberCardController.java
    
    creates route to borrow book(s).
    
    Reviewed-by: damou_ <mouadsehbaoui@gmail.com>

--- src/main/java/com/example/demo/memberCard/MemberCardController.java
+++ src/main/java/com/example/demo/memberCard/MemberCardController.java
@@ -6,9 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Data
 @Validated
@@ -37,6 +35,11 @@ public class MemberCardController {
     @PostMapping(path = "/{memberCardUUID}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MemberCard> postMemberCard(@PathVariable("memberCardUUID") UUID memberCardUUID) {
         return memberCardService.postMemberCard(memberCardUUID);
+    }
+
+    @PostMapping(path = "/{memberCardUUID}/borrow", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> postBorrowBooks(@PathVariable("memberCardUUID") UUID memberCardUUID, @RequestBody Map<Object, ArrayList<UUID>> booksArrayJson) {
+        return memberCardService.borrowBooks(memberCardUUID, booksArrayJson);
     }
 
 }
