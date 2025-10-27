6662a41e06a1bc4b177e99e51318e31c56327dde
damouu <mouadsehbaoui@gmail.com>
2025-10-27 11:52:04 +0100

    #SD-232 Done Unassigned TestsPassed Done
    refactor: BookStudentRepository.java
    
    adds existsByBookIsBorrowed sql query function.
    
    Reviewed-by: damou_ <mouadsehbaoui@gmail.com>

--- src/main/java/com/example/demo/book/BookStudentRepository.java
+++ src/main/java/com/example/demo/book/BookStudentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface BookStudentRepository extends JpaRepository<BookMemberCard, Integer>, JpaSpecificationExecutor<BookMemberCard> {
@@ -16,5 +17,9 @@ public interface BookStudentRepository extends JpaRepository<BookMemberCard, Int
 
     @Query("SELECT b FROM BookMemberCard b WHERE b.book.id = :id  and b.borrow_return_date = null ")
     Optional<BookMemberCard> findBookStudentByIDUpdate(int id);
+
+
+    @Query(value = "SELECT EXISTS (select 1 from book_member_card inner join book on book.id = book_member_card.book_id where book.book_uuid = :book_uuid AND book_member_card.borrow_return_date is null ) AS is_borrowed", nativeQuery = true)
+    boolean existsByBookIsBorrowed(UUID book_uuid);
 
 }
