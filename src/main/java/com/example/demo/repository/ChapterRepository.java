e887f84843930447ef034a10c414421be5752653
damouu <mouadsehbaoui@gmail.com>
2025-11-09 22:14:47 +0100

    #SD-235 Done Unassigned Fixed Done
    creates: ChapterRepository.java
    
    creates repository layer for chapters.
    
    Reviewed-by: damou_ <mouadsehbaoui@gmail.com>

---
+++ src/main/java/com/example/demo/repository/ChapterRepository.java
@@ -0,0 +1,29 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Chapter;
+import com.example.demo.model.Series;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChapterRepository extends JpaRepository<Chapter, Integer>, JpaSpecificationExecutor<Chapter> {
+
+    Page<Chapter> findAll(Specification<Chapter> specification, Pageable pageable);
+
+    @Query("SELECT b FROM chapter b WHERE b.series = :series ORDER BY b.created_at ASC ")
+    List<Chapter> findRecentChapterBySeries(@Param("series") Series series, Pageable pageable);
+
+
+    @Query("SELECT b FROM chapter b WHERE b.series = :series and b.deleted_at is null ")
+    Page<Chapter> findSeriesBySeries_uuidAndChapters(@Param("series") Series series, Pageable pageable);
+
+
+}
