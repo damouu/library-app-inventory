174f6708dbc4aad2435bd99404cc1d866ddac75c
damouu <mouadsehbaoui@gmail.com>
2025-12-07 17:59:04 +0100

    refactor: BookRepository.java (#LAI-1)

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Book;
-import com.example.demo.model.Chapter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,13 +18,12 @@ import java.util.UUID;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book> {
 
-    @Query("SELECT b FROM book b WHERE b.deleted_at is null and b.bookUUID = :uuid")
-    Optional<Book> findByUuid(UUID uuid);
-
+    Optional<Book> findFirstByChapterUUIDAndDeletedDateIsNullAndCurrentlyBorrowedIsFalse(UUID chapterUUID);
 
     Page<Book> findAll(Specification<Book> specification, Pageable pageable);
 
-    @Query("SELECT b FROM book b WHERE b.chapter = :chapter and b.deleted_at is null and b.currentlyBorrowed is false order by b.addedDate desc ")
-    List<Book> findRecentBooksByChapter(@Param("chapter") Chapter chapter, Pageable pageable);
+    @Modifying
+    @Query("UPDATE book b SET b.currentlyBorrowed = :status WHERE b.bookUUID IN :bookUuids")
+    void updateBorrowedStatusInBatch(@Param("bookUuids") List<UUID> bookUuids, @Param("status") boolean status);
 
 }
