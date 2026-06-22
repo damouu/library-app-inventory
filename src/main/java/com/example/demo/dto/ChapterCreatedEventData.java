b0ac673f9beee80e6dc5f0c26a4ec1f9ed3f789e
damouu <mouadsehbaoui@gmail.com>
2026-06-22 14:09:54 +0900

    Refactor: ChapterCreatedEventData (#LAI-15)

--- src/main/java/com/example/demo/dto/ChapterCreatedEventData.java
+++ src/main/java/com/example/demo/dto/ChapterCreatedEventData.java
@@ -1,37 +1,27 @@
 package com.example.demo.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
-
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@Data
-@Builder
-@AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class ChapterCreatedEventData {
 
-    private UUID chapter_uuid;
+public record ChapterCreatedEventData(
+        UUID chapter_uuid,
 
-    private UUID series_uuid;
+        UUID series_uuid,
 
-    private String title;
+        String title,
 
-    private String second_title;
+        String second_title,
 
-    private Integer total_pages;
+        Integer total_pages,
 
-    private Integer chapter_number;
+        Integer chapter_number,
 
-    private String summary;
+        String summary,
 
-    private String cover_artwork_url;
+        String cover_artwork_url,
 
-    private String publication_date;
-    
-    private Integer initial_copies_count;
+        String publication_date,
 
+        Integer initial_copies_count
+) {
 }
