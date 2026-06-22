4e448b3c9722b0e34013aaf722eeb4bb8cb169d2
damouu <mouadsehbaoui@gmail.com>
2026-06-22 14:09:03 +0900

    Refactor: BorrowCreatedEventData (#LAI-15)

--- src/main/java/com/example/demo/dto/BorrowCreatedEventData.java
+++ src/main/java/com/example/demo/dto/BorrowCreatedEventData.java
@@ -1,24 +1,18 @@
 package com.example.demo.dto;
 
-import lombok.*;
-
 import java.util.List;
 import java.util.UUID;
 
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class BorrowCreatedEventData {
 
-    private UUID member_card_uuid;
+public record BorrowCreatedEventData(
+        UUID member_card_uuid,
 
-    private UUID borrow_uuid;
+        UUID borrow_uuid,
 
-    private String borrow_start_date;
+        String borrow_start_date,
 
-    private String borrow_end_date;
+        String borrow_end_date,
 
-    private List<BorrowedItem> borrowed_items;
+        List<BorrowedItem> borrowed_items
+) {
 }
